6db83e0536155c0bb1b1ed144e7e159164d4fed7
LawrenceHan <yangfei6565@163.com>
2017-09-13 23:32:22 +0800

    upadte

--- GeoQuiz/app/src/main/java/com/example/hanguang/geoquiz/Question.java
+++ GeoQuiz/app/src/main/java/com/example/hanguang/geoquiz/Question.java
@@ -7,6 +7,7 @@ package com.example.hanguang.geoquiz;
 public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mAnswered;
 
     public Question(int textResId, boolean answerTrue) {
         mTextResId = textResId;
@@ -27,5 +28,13 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswered() {
+        return mAnswered;
+    }
+
+    public void setAnswered(boolean answered) {
+        mAnswered = answered;
     }
 }
--- GeoQuiz/app/src/main/java/com/example/hanguang/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/hanguang/geoquiz/QuizActivity.java
@@ -1,18 +1,31 @@
 package com.example.hanguang.geoquiz;
 
+import android.annotation.SuppressLint;
+import android.os.PersistableBundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final String KEY_Questions = "questions";
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -26,14 +39,27 @@ public class QuizActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +77,20 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int index = mCurrentIndex - 1;
+                if (index < 0) {
+                    index = mQuestionBank.length-1;
+                }
+                mCurrentIndex = index;
+                updateQuestion();
+            }
+        });
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,28 +102,79 @@ public class QuizActivity extends AppCompatActivity {
         updateQuestion();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     private void updateQuestion() {
+//        Log.d(TAG, "Updating question text ", new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
 
     private void checkAnswer(boolean userPressedTrue) {
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        Question question = mQuestionBank[mCurrentIndex];
+        if (!question.isAnswered()) {
+            question.setAnswered(true);
 
-        int messageResId = 0;
+            boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+            int messageResId = 0;
+
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
+
+            Toast toast = Toast.makeText(
+                    this,
+                    messageResId,
+                    Toast.LENGTH_SHORT
+            );
+            toast.setGravity(Gravity.TOP, 0, 0);
+            toast.show();
         } else {
-            messageResId = R.string.incorrect_toast;
+            Toast toast = Toast.makeText(
+                    this,
+                    "You have already answered.",
+                    Toast.LENGTH_SHORT
+            );
+            toast.setGravity(Gravity.TOP, 0, 0);
+            toast.show();
         }
-
-        Toast toast = Toast.makeText(
-                this,
-                messageResId,
-                Toast.LENGTH_SHORT
-        );
-        toast.setGravity(Gravity.TOP, 0, 0);
-        toast.show();
     }
 }
